d4cc0116841ba7dfbe09f538aa182ee056ba406b
emanuel-schmid <schmide@ethz.ch>
2023-12-01 15:59:17 +0100

    fix tests

--- climada/hazard/test/test_trop_cyclone.py
+++ climada/hazard/test/test_trop_cyclone.py
@@ -44,7 +44,7 @@ DATA_DIR = Path(hazard_test.__file__).parent.joinpath('data')
 TEST_TRACK = DATA_DIR.joinpath("trac_brb_test.csv")
 TEST_TRACK_SHORT = DATA_DIR.joinpath("trac_short_test.csv")
 
-CENTR_TEST_BRB = Centroids.from_hdf5(get_test_file(name='centr_test_brb', format='hdf5'))
+CENTR_TEST_BRB = Centroids.from_hdf5(get_test_file('centr_test_brb', file_format='hdf5'))
 
 
 class TestReader(unittest.TestCase):
